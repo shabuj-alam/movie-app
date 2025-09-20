6b683a20d456acf4a2e703f7aee10ce8fbb51409
Shabuj <shabujalam13@gmail.com>
2025-09-20 16:49:46 +0100

    implement Navigation bar and functionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,64 @@
 import {Tabs} from "expo-router";
+import {Image, ImageBackground, Text, View} from "react-native";
+import {images} from "@/constants/images";
+import {icons} from "@/constants/icons";
+
+const TabIcon = ({focused, icon, title}: any) => {
+    if(focused) {
+        return (
+            <ImageBackground
+                source={images.highlight}
+                className="flex flex-1 flex-row justify-center items-center w-full min-w-[112px] min-h-16 mt-4 rounded-full overflow-hidden"
+            >
+                <Image source={icon} tintColor='#151312' className="size-5"/>
+                <Text className="text-secondary text-base font-semibold ml-2 ">{title}</Text>
+            </ImageBackground>
+        )
+    }
+
+    return (
+        <View className="size-full justify-center items-center mt-4 rounded-full">
+            <Image source={icon} tintColor='#A8b5DB' className="size-5"/>
+        </View>
+    )
+}
 
 const _Layout = () => {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                },
+                tabBarStyle: {
+                    backgroundColor: '#0f0D23',
+                    borderRadius: 50,
+                    marginHorizontal: 20,
+                    marginBottom:36,
+                    height: 52,
+                    position: "absolute",
+                    overflow: "hidden",
+                    borderWidth: 1,
+                    borderColor: "#0f0d23",
+                }
+            }}
+        >
             <Tabs.Screen
                 name="index"
                 options={{
                     name: "Home",
                     headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.home}
+                            title="Home"
+                        />
+                    )
                 }}
             />
             <Tabs.Screen
@@ -15,6 +66,13 @@ const _Layout = () => {
                 options={{
                     name: "Search",
                     headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.search}
+                            title="Search"
+                        />
+                    )
                 }}
             />
             <Tabs.Screen
@@ -22,6 +80,13 @@ const _Layout = () => {
                 options={{
                     name: "Saved",
                     headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.save}
+                            title="Save"
+                        />
+                    )
                 }}
             />
             <Tabs.Screen
@@ -29,6 +94,13 @@ const _Layout = () => {
                 options={{
                     name: "Profile",
                     headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon
+                            focused={focused}
+                            icon={icons.person}
+                            title="Profile"
+                        />
+                    )
                 }}
             />
         </Tabs>
